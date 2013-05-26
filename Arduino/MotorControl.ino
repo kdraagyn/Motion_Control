07f086f5ebc71f6c1442fcf69c17bb3a18d20726
Keith Nygaard <kenygaar@mines.edu>
2013-05-25 21:42:05 -0600

    adding arduino sketch

---
+++ Arduino/MotorControl.ino
@@ -0,0 +1,13 @@
+/*
+  A program to control 3 motors on an arduino given input from a processing program.
+  
+  Start date: 5/25/2013
+*/
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  
+}
